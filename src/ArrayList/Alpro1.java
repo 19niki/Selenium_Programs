7f88c80f0eea1fc45ecfad5642f1a9ccdb202188
nikhil <nikhilravi9847@gmail.com>
2022-06-28 21:10:29 +0530

    code change

--- src/ArrayList/Alpro1.java
+++ src/ArrayList/Alpro1.java
@@ -53,8 +53,10 @@ package ArrayList;
 
 public class Alpro1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+{
 		// TODO Auto-generated method stub
+              ArrayList al = new ArrayList();     
 
 	}
 
